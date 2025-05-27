015b97a76dd753d7721c6ad55633b8dcad8bdefd
Kevin Picón <42751420+KevinPicon01@users.noreply.github.com>
2025-05-27 13:36:50 -0500

    Init

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+    title: "Smash Point | Smash Burger gigante en Piedecuesta",
+    description: "Smash Point es tu burger dealer de confianza. Una sola smash burger, gigante, brutal y con ingredientes top. Estamos en el Edificio Smart Junín, Piedecuesta. Pide por WhatsApp o Instagram. Local pequeño, hamburguesa gigante.",
+};
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
