1798369e6181172cabbf7e02ff10298a099f7c18
Kevin Picón <42751420+KevinPicon01@users.noreply.github.com>
2025-07-01 23:40:43 -0500

    Update menu.tsx

--- components/menu.tsx
+++ components/menu.tsx
@@ -7,6 +7,30 @@ export default function Menu() {
 
             <div className="menu-main-container">
                 <div className="menu"></div>
+                <div style={{ position: 'absolute', left: '-9999px', top: '-9999px' }}>
+                    <h2>Menú Smash Point</h2>
+
+                    <h3>Hamburguesas</h3>
+                    <p>Incluye papas. Pan, queso cheddar, carne smash, salsa especial de la casa, tocineta y cebolla caramelizada.</p>
+                    <ul>
+                        <li>Sencilla – $22.000</li>
+                        <li>Doble (todos los ingredientes x2) – $28.000</li>
+                        <li>Triple (todos los ingredientes x3) – $32.000</li>
+                    </ul>
+
+                    <h3>Adicionales</h3>
+                    <ul>
+                        <li>Papas On Point (cheddar y tocineta) – $6.000</li>
+                        <li>Extra Cheddar – $3.000</li>
+                        <li>Extra Tocineta – $4.000</li>
+                    </ul>
+
+                    <h3>Bebidas</h3>
+                    <ul>
+                        <li>Coca-Cola – $4.000</li>
+                        <li>Coca-Cola Zero – $4.000</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
